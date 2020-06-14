e85ca97ffa80ac873d0241803cf2bb0576056dfa
Dheeraj Hiremath <hiremath.d@husky.neu.edu>
2020-06-14 11:59:24 -0400

    Final

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,2 +1,11 @@
+import axios from "axios";
 const KEY = "AIzaSyCgta5oemX_Y4zAxGauLJ8acRjcF0dkyQE";
 
+export default axios.create({
+  baseURL: " https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
